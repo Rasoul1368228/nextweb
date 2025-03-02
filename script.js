53fb6c708c4c34303b208e7c98f9a784fc13acf7
Rasoul1368228 <rasoul6856@gmail.com>
2025-03-02 12:10:59 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// تابع برای باز و بسته کردن منو موبایل
+function toggleMenu() {
+        document.getElementById('mobile-menu').classList.toggle('open');
+        document.getElementById('overlay').classList.toggle('open');
+    }
+    
+    // بستن منو با کلیک روی Overlay
+    document.getElementById('overlay').addEventListener('click', () => {
+        document.getElementById('mobile-menu').classList.remove('open');
+        document.getElementById('overlay').classList.remove('open');
+    });
+    
+    // اضافه کردن رویداد به منو همبرگری برای باز و بسته کردن منو
+    document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu);
+    
